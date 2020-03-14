60421d9584f9b3740f4ae949432ae27f19e50840
gillchristian <gillchristiang@gmail.com>
2020-03-14 19:11:08 +0100

    build: :package:

---
+++ docs/precache-manifest.5467ffd042cfd20dde7a285a64894a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326ebebff465ab377b85dca7f9c753e9",
+    "url": "/literate-avocado/favicon.ico"
+  },
+  {
+    "revision": "6edb4786689350e7d630ca5bc0c31582",
+    "url": "/literate-avocado/index.html"
+  },
+  {
+    "revision": "a46e76204ef6c34b940cd440a30451d2",
+    "url": "/literate-avocado/manifest.json"
+  },
+  {
+    "revision": "d9670d90c04b75a72a59",
+    "url": "/literate-avocado/static/js/main.d9670d90.chunk.js"
+  },
+  {
+    "revision": "26f742a5647b94f72c11",
+    "url": "/literate-avocado/static/js/runtime~main.26f742a5.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/literate-avocado/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
